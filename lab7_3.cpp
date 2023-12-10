8f45714e3c5e518331aecd86484b8de037df33dc
suchanun <suchaun_j@cmu.ac.th>
2023-12-11 04:42:21 +0700

    https://github.com/Jsuchanun/lab7

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,13 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+//ไม่ต้องเติม function main()
 
-char before(char x){
-	//Write your function definition here
-}
+char before(char input) {
+    
+    if (input >= 'A' && input <= 'Z') {
+        return (input == 'A') ? 'Z' : input - 1;
+    } else {
+        return '0';
+    }
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    return 0;
 }
